1df86282119ea359f274fdb23fe5efefd443910f
加藤創貴 <katouhajimetaka@souki.local>
2021-09-29 18:17:15 +0900

    first commitls

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import SignUp from './components/SignUp';
+import Home from './components/Home';
+import Login from './components/Login';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        <BrowserRouter>
+          <PrivateRoute exact path="/" component={Home} />
+          <PublicRoute path="/signup" component={SignUp} />
+          <PublicRoute path="/login" component={Login} />
+        </BrowserRouter>
+      </div>
+    </AuthProvider>
   );
 }
 
